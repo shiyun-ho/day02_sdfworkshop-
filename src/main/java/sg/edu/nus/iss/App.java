002ae8e50a2195a83b1f976bd06fd33cb56415e3
shiyun-ho <serene.hoshiyun@gmail.com>
2023-01-16 15:24:24 +0800

    create log in file

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -2,6 +2,7 @@ package sg.edu.nus.iss;
 
 import java.io.Console;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -16,11 +17,12 @@ public class App {
      * Says hello to the world.
      * @param args The arguments of the program.
      */
+    
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //specify directory
         //create a folder
-        String dirPath = "\\data2";
+        String dirPath = ".\\";
         //create file with empty name as it will be within main function
         String fileName = "";
 
@@ -40,7 +42,7 @@ public class App {
         String input = ""; 
 
         while (!input.equals("quit")){
-            input.con.readLine("What do you want to perform? Type 'quit' to exit program.");
+            input = con.readLine("What do you want to perform? Type 'quit' to exit program.");
             
             switch (input) {
                 case "help":
@@ -52,19 +54,32 @@ public class App {
                     displayMessage("quit to quit");
                     break;
                 case "list":
-                //creates list
-                    if (cartItems.size() > 0 ){
-                        for (String item: cartItems) {
-                            System.out.printf(item);
-                        }
-                    } else {
-                        System.out.println("Your cart is empty!");
-                    }
-                    break;
+                    listCart(cartItems);
                 case "user":
                     break; 
                 default:
-                    System.out.println("Hello");
+            }
+
+            
+            if (input.startsWith("login")){
+                input = input.replace(",", " ");
+                
+                String strLogin = " ";
+                
+                Scanner scanner = new Scanner(input.substring(6));
+
+                while (scanner.hasNext()){
+                    fileName = scanner.next();}
+                
+                File loginFile = new File(dirPath + File.separator + fileName);
+
+                boolean isCreated = loginFile.createNewFile(); 
+
+                if (isCreated){
+                    displayMessage("New file created successfully" + loginFile.getCanonicalPath());
+                } else {
+                    displayMessage("File already created!");
+                }
             }
 
             if (input.startsWith("add")){
@@ -82,13 +97,49 @@ public class App {
                     cartItems.add(strValue);
                 }
             }
+
+            if (input.startsWith("delete")){
+                cartItems = deleteCartItem(cartItems, input);
+            }
         }
 
-        public static void displayMessage(String message){
-            System.out.println(message);
+        public static List<String> deleteCartItem(List<String> cartItems, String input){
+            String strValue = "";
+            Scanner scanner = new Scanner(input.substring(6));
+
+                while (scanner.hasNext()){
+                    strValue = scanner.next();
+                    int listItemIndex = Integer.parseInt(strValue);
+
+                    if (listItemIndex < cartItems.size()){
+                        cartItems.remove(listItemIndex);
+                    } else {
+                        displayMessage("Incorrect item index.");
+                    }
+                }
+            return cartItems;
         }
+        public static void listCart(List<String> cartItems){
+            if (cartItems.size()>0){
+                // for (String item: cartItems){
+                //     System.out.println(item);
+                // }
+                for (int i =0; i < cartItems.size(), i++){
+                    System.out.printf("%d: %s\n", i, cartItems.get(i));
+                } 
+            } else {
+                    System.out.println("Your cart is empty.");
+                }
+        }
+        
         
         
 
     }
+    
+    public static void displayMessage(String message){
+                System.out.println(message);
+            }
+
+    
 }
